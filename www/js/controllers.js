7903b16b0043be04cf49d5c448a8e15184e4fc14
SufiyanMansoor <sufiyanbaw1256@gmail.com>
2016-04-12 16:08:24 +0500

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,79 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+.controller('AuthCtrl', function($scope, $rootScope, $firebaseAuth, $cordovaOauth, $state){
+  var fb_app_id = '1416319645359452';
+  var ref = new Firebase("http://giphy.firebaseio.com/");
+  var auth = $firebaseAuth(ref);
+  $rootScope.user = null;
+
+  $scope.login = function() {
+    $cordovaOauth.facebook(fb_app_id, ["email"]).then(function(result) {
+      auth.$authWithOAuthToken("facebook", result.access_token).then(function(authData) {
+        console.log(JSON.stringify(authData));
+
+        $rootScope.user = authData;
+        $rootScope.$apply();
+        $state.go('app.browse');
+
+      }, function(error) {
+        console.error("ERROR: " + error);
+      });
+    }, function(error) {
+      console.log("ERROR: " + error);
+    });
+  };
+})
